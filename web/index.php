dbaf7f92b0e7812c818fab8834ffbfa8bae755f5
Tyler Sommer <sommertm@gmail.com>
2014-02-22 17:30:39 -0800

    Switched index action over to nice framework

--- web/index.php
+++ web/index.php
@@ -4,53 +4,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Symfony\Component\Debug\Debug::enable();
 
-$routeDispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-        $r->addRoute('POST', '/', 'handler1');
-        $r->addRoute('PUT', '/', 'handler1');
-        $r->addRoute('GET', '/test', 'handler2');
-    });
+$routeFactory = function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', function(\Symfony\Component\HttpFoundation\Request $request) {
+            $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../views');
+            $twig = new \Twig_Environment($loader);
+            
+            $mtime = new DateTime('@' . filemtime(__DIR__ . '/packages.json'));
 
-$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+            return new \Symfony\Component\HttpFoundation\Response(
+                $twig->render('index.html.twig', array(
+                    'updatedAt' => $mtime,
+                    'name'      => 'Terramar Labs'
+                )));
+        });
+};
 
-$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-$resolver = new \Symfony\Component\HttpKernel\Controller\ControllerResolver();
-$kernel = new \Symfony\Component\HttpKernel\HttpKernel($dispatcher, $resolver);
-
-$dispatcher->addListener('kernel.request', function(\Symfony\Component\HttpKernel\Event\GetResponseEvent $event) use ($routeDispatcher) {
-        $request = $event->getRequest();
-        $routeInfo = $routeDispatcher->dispatch($request->getMethod(), $request->getPathInfo());
-        switch ($routeInfo[0]) {
-            case FastRoute\Dispatcher::NOT_FOUND:
-                $message = sprintf('No route found for "%s %s"', $request->getMethod(), $request->getPathInfo());
-
-                throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException($message);
-
-            case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-                $allowedMethods = $routeInfo[1];
-                $message = sprintf('No route found for "%s %s": Method Not Allowed (Allow: %s)', $request->getMethod(), $request->getPathInfo(), implode(', ', $allowedMethods));
-
-                throw new \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException($allowedMethods, $message);
-
-            case FastRoute\Dispatcher::FOUND:
-                $handler = $routeInfo[1];
-                $vars = $routeInfo[2];
-
-                $request->attributes->set('_route_params', $vars);
-                $request->attributes->set('_controller', $handler);
-                
-                break;
-        }
-    });
-
-function handler1() {
-    echo "handler1 got fired";
-}
-
-function handler2() {
-    echo "handler2 got fired";
-}
-
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+$app = new \TylerSommer\Nice\Application($routeFactory);
+$app->run();
