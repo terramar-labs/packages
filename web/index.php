ecc8af0828fdfa35a74bdd0e5b4497550ed66189
Tyler Sommer <sommertm@gmail.com>
2014-02-28 11:10:13 -0800

    Updated front controller with changes in nice

--- web/index.php
+++ web/index.php
@@ -2,24 +2,24 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use TylerSommer\Nice\Application;
+use Nice\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Symfony\Component\Debug\Debug::enable();
 
-$app = new Application();
-
-$app->setParameter('twig.template_dir', __DIR__ . '/../views');
+$app = new Application('dev', true);
 
 $app->set('routes', function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/', function(Application $app, Request $request) {
             $mtime = new DateTime('@' . filemtime(__DIR__ . '/packages.json'));
 
+            $config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/../config.yml'));
+            
             return new Response(
                 $app->get('twig')->render('index.html.twig', array(
                         'updatedAt' => $mtime,
-                        'name'      => 'Terramar Labs'
+                        'name'      => $config['satis']['name']
                     )));
         });
 });
