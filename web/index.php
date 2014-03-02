f5a93facaedcd0625ebe003e6a9fea230957b5a0
Tyler Sommer <sommertm@gmail.com>
2014-03-01 19:17:15 -0800

    Added creation of TwigExtension

--- web/index.php
+++ web/index.php
@@ -3,19 +3,20 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Nice\Application;
+use Nice\Extension\TwigExtension;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Symfony\Component\Debug\Debug::enable();
 
 $app = new Application('dev', true);
-
-$app->set('routes', function(FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/', function(Application $app, Request $request) {
+$app->registerExtension(new TwigExtension(__DIR__ . '/../views'));
+$app->set('routes', function (FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', function (Application $app, Request $request) {
             $mtime = new DateTime('@' . filemtime(__DIR__ . '/packages.json'));
 
             $config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/../config.yml'));
-            
+
             return new Response(
                 $app->get('twig')->render('index.html.twig', array(
                         'updatedAt' => $mtime,
