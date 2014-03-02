0ea83bb0d22472637b83801231e1fac889876828
Tyler Sommer <sommertm@gmail.com>
2014-03-02 14:00:17 -0800

    Updated main index

--- web/index.php
+++ web/index.php
@@ -2,27 +2,11 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Nice\Application;
-use Nice\Extension\TwigExtension;
+use Terramar\Packages\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Symfony\Component\Debug\Debug::enable();
 
 $app = new Application('dev', true);
-$app->registerExtension(new TwigExtension(__DIR__ . '/../views'));
-$app->set('routes', function (FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/', function (Application $app, Request $request) {
-            $mtime = new DateTime('@' . filemtime(__DIR__ . '/packages.json'));
-
-            $config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/../config.yml'));
-
-            return new Response(
-                $app->get('twig')->render('index.html.twig', array(
-                        'updatedAt' => $mtime,
-                        'name'      => $config['satis']['name']
-                    )));
-        });
-});
-
 $app->run();
