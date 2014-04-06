40aa3bec66d69547a17763b1b55d84804452e5d7
Tyler Sommer <sommertm@gmail.com>
2014-04-05 18:12:49 -0700

    Implemented index action on ManageController

--- src/Terramar/Packages/Controller/ManageController.php
+++ src/Terramar/Packages/Controller/ManageController.php
@@ -11,21 +11,6 @@ class ManageController
 {
     public function indexAction(Application $app, Request $request)
     {
-        $config = Yaml::parse(file_get_contents($app->getRootDir() . '/config.yml'));
-        
-        return new Response(
-            $app->get('twig')->render('manage.html.twig', array(
-                    'name'      => $config['satis']['name']
-                )));
-    }
-
-    public function loginAction(Application $app, Request $request)
-    {
-        $config = Yaml::parse(file_get_contents($app->getRootDir() . '/config.yml'));
-
-        return new Response(
-            $app->get('twig')->render('login.html.twig', array(
-                    'name'      => $config['satis']['name']
-                )));
+        return new Response($app->get('twig')->render('Manage/index.html.twig'));
     }
 }
