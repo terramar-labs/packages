8b78b09af4515886e7f5c719ce872398543ef726
Tyler Sommer <sommertm@gmail.com>
2014-04-06 08:57:26 -0700

    Implemented basic dashboard

--- src/Terramar/Packages/Controller/ManageController.php
+++ src/Terramar/Packages/Controller/ManageController.php
@@ -11,6 +11,11 @@ class ManageController
 {
     public function indexAction(Application $app, Request $request)
     {
-        return new Response($app->get('twig')->render('Manage/index.html.twig'));
+        return new Response($app->get('twig')->render('Manage/index.html.twig', array(
+                    'commits' => 11094,
+                    'packages' => 29,
+                    'releases' => 325,
+                    'installs' => 4345,
+                )));
     }
 }
