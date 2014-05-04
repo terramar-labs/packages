edef0760387fbff2356977044f72f1f9196fc146
Tyler Sommer <sommertm@gmail.com>
2014-05-04 16:56:35 -0700

    Updated routecollector with new routes

--- src/Terramar/Packages/Router/RouteCollector.php
+++ src/Terramar/Packages/Router/RouteCollector.php
@@ -15,11 +15,14 @@ class RouteCollector extends BaseCollector
     {
         $this->addNamedRoute('home', 'GET', '/', 'Terramar\Packages\Controller\DefaultController::indexAction');
         $this->addNamedRoute('login', 'GET', '/login', 'Terramar\Packages\Controller\DefaultController::loginAction');
+        $this->addNamedRoute('logout', 'GET', '/logout', '');
 
         $this->addNamedRoute('manage', 'GET', '/manage', 'Terramar\Packages\Controller\ManageController::indexAction');
 
-        $this->addNamedRoute('manage_packages', 'GET', '/packages', 'Terramar\Packages\Controller\PackageController::indexAction');
-        $this->addNamedRoute('manage_package_new', 'GET', '/package/new', 'Terramar\Packages\Controller\PackageController::newAction');
-        $this->addNamedRoute('manage_package_create', 'POST', '/package/create', 'Terramar\Packages\Controller\PackageController::createAction');
+        $this->addNamedRoute('manage_packages', 'GET', '/manage/packages', 'Terramar\Packages\Controller\PackageController::indexAction');
+        $this->addNamedRoute('manage_package_new', 'GET', '/manage/package/new', 'Terramar\Packages\Controller\PackageController::newAction');
+        $this->addNamedRoute('manage_package_create', 'POST', '/manage/package/create', 'Terramar\Packages\Controller\PackageController::createAction');
+        $this->addNamedRoute('manage_package_edit', 'GET', '/manage/package/{id}/edit', 'Terramar\Packages\Controller\PackageController::editAction');
+        $this->addNamedRoute('manage_package_update', 'POST', '/manage/package/{id}/update', 'Terramar\Packages\Controller\PackageController::updateAction');
     }
 }
