ac84fc25da52dd412f8db2075f08115cce9b3243
Tyler Sommer <sommertm@gmail.com>
2014-05-12 09:56:32 -0700

    Package and Configuration are now entities

---
+++ src/Terramar/Packages/Entity/Configuration.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Terramar\Packages\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="configurations")
+ */
+class Configuration
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="url", type="string")
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    private $enabled = true;
+
+    /**
+     * @ORM\Column(name="token", type="string")
+     */
+    private $token;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Terramar\Packages\Entity\Configuration")
+     * @ORM\JoinColumn(name="configuration_id", referencedColumnName="id")
+     */
+    private $configuration;
+
+    /**
+     * @return boolean
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param boolean $enabled
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = (bool) $enabled;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param mixed $token
+     */
+    public function setToken($token)
+    {
+        $this->token = (string) $token;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = (string) $url;
+    }
+}
---
+++ src/Terramar/Packages/Entity/Package.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Terramar\Packages\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity 
+ * @ORM\Table(name="packages")
+ */
+class Package
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="description", type="string")
+     */
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = (string) $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+    }
+}
