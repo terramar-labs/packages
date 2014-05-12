fa0b2e5b1b06644cf7d2f3c6e82dd5ade8ece705
Tyler Sommer <sommertm@gmail.com>
2014-05-12 10:21:41 -0700

    Updated controllers to use orm

--- src/Terramar/Packages/Controller/ManageController.php
+++ src/Terramar/Packages/Controller/ManageController.php
@@ -6,16 +6,22 @@ use Nice\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Yaml\Yaml;
+use Terramar\Packages\Repository\PackageRepository;
 
 class ManageController
 {
     public function indexAction(Application $app, Request $request)
     {
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $app->get('doctrine.orm.entity_manager');
+        $packages = $entityManager->getRepository('Terramar\Packages\Entity\Package')->createQueryBuilder('p')
+            ->select('COUNT(p)')
+            ->getQuery()->getSingleScalarResult();
+
         return new Response($app->get('twig')->render('Manage/index.html.twig', array(
                     'commits' => 11094,
-                    'packages' => 29,
+                    'packages' => $packages,
                     'releases' => 325,
-                    'installs' => 4345,
                 )));
     }
 }
--- src/Terramar/Packages/Controller/PackageController.php
+++ src/Terramar/Packages/Controller/PackageController.php
@@ -6,18 +6,16 @@ use Nice\Application;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Yaml\Yaml;
-use Terramar\Packages\Model\Package;
-use Terramar\Packages\Repository\PackageRepository;
+use Terramar\Packages\Entity\Package;
 
 class PackageController
 {
     public function indexAction(Application $app, Request $request)
     {
-        /** @var PackageRepository $repository */
-        $repository = $app->get('repository.package');
-        
-        $packages = $repository->findAll();
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $app->get('doctrine.orm.entity_manager');
+
+        $packages = $entityManager->getRepository('Terramar\Packages\Entity\Package')->findAll();
         
         return new Response($app->get('twig')->render('Package/index.html.twig', array(
                     'packages' => $packages
@@ -37,18 +35,19 @@ class PackageController
         $package->setName($request->get('name'));
         $package->setDescription($request->get('description'));
 
-        /** @var PackageRepository $repository */
-        $repository = $app->get('repository.package');
-        $repository->save($package);
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $app->get('doctrine.orm.entity_manager');
+        $entityManager->persist($package);
+        $entityManager->flush();
         
         return new RedirectResponse($app->get('router.url_generator')->generate('manage_packages'));
     }
 
     public function editAction(Application $app, $id)
     {
-        /** @var PackageRepository $repository */
-        $repository = $app->get('repository.package');
-        $package = $repository->findById($id);
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $app->get('doctrine.orm.entity_manager');
+        $package = $entityManager->getRepository('Terramar\Packages\Entity\Package')->find($id);
         if (!$package) {
             throw new \RuntimeException('Oops');
         }
@@ -60,13 +59,14 @@ class PackageController
 
     public function updateAction(Application $app, Request $request, $id)
     {
-        /** @var PackageRepository $repository */
-        $repository = $app->get('repository.package');
-        $package = $repository->findById($id);
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $app->get('doctrine.orm.entity_manager');
+        $package = $entityManager->getRepository('Terramar\Packages\Entity\Package')->find($id);
         $package->setName($request->request->get('name'));
         $package->setDescription($request->request->get('description'));
 
-        $repository->save($package);
+        $entityManager->persist($package);
+        $entityManager->flush();
 
         return new RedirectResponse($app->get('router.url_generator')->generate('manage_packages'));
     }
