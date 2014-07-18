59e10b42e6de4e9ea7ed50f9596a2596324b6cfd
Tyler Sommer <sommertm@gmail.com>
2014-07-17 19:58:12 -0700

    Defined additional events

--- src/Terramar/Packages/Events.php
+++ src/Terramar/Packages/Events.php
@@ -5,7 +5,22 @@ namespace Terramar\Packages;
 final class Events
 {
     /**
-     * Dispatched when a remote project host receives an update
+     * Dispatched when a package is created for a project
+     */
+    const PACKAGE_CREATE = 'package.create';
+    
+    /**
+     * Dispatched when a package's source code is updated
      */
     const PACKAGE_UPDATE = 'package.update';
+
+    /**
+     * Dispatched when a package is enabled
+     */
+    const PACKAGE_ENABLE = 'package.enable';
+
+    /**
+     * Dispatched when a package is disabled
+     */
+    const PACKAGE_DISABLE = 'package.disable';
 }
