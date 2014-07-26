d6e60d6972eb820a562c3e082a3c6a03f1a46876
Tyler Sommer <sommertm@gmail.com>
2014-07-26 04:35:15 -0700

    Added new services registration in PackagesExtension

--- src/DependencyInjection/PackagesExtension.php
+++ src/DependencyInjection/PackagesExtension.php
@@ -3,10 +3,9 @@
 namespace Terramar\Packages\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Reference;
-use Terramar\Packages\Events;
-use Terramar\Packages\Plugin\ContainerConfiguratorInterface;
 use Terramar\Packages\Plugin\PluginInterface;
 
 class PackagesExtension extends Extension
@@ -31,7 +30,7 @@ class PackagesExtension extends Extension
         $this->plugins = $plugins;
         $this->options = $options;
     }
-    
+
     /**
      * Returns extension configuration
      *
@@ -44,11 +43,11 @@ class PackagesExtension extends Extension
     {
         return new PackagesConfiguration();
     }
-    
+
     /**
      * Loads a specific configuration.
      *
-     * @param array            $configs    An array of configuration values
+     * @param array            $configs   An array of configuration values
      * @param ContainerBuilder $container A ContainerBuilder instance
      *
      * @throws \InvalidArgumentException When provided tag is not defined in this extension
@@ -62,28 +61,67 @@ class PackagesExtension extends Extension
         $container->register('router.collector', 'Terramar\Packages\Router\RouteCollector')
             ->addArgument(new Reference('router.parser'))
             ->addArgument(new Reference('router.data_generator'));
-        
+
         $container->register('packages.helper.sync', 'Terramar\Packages\Helper\SyncHelper')
             ->addArgument(new Reference('event_dispatcher'));
-        
+
         $container->setParameter('packages.configuration', array(
                 'output_dir' => $config['output_dir']
             ));
-        
+
         $container->register('packages.helper.resque', 'Terramar\Packages\Helper\ResqueHelper');
-        
+
         $container->setParameter('packages.resque.host', $config['resque']['host']);
         $container->setParameter('packages.resque.port', $config['resque']['port']);
         $container->setParameter('packages.resque.database', $config['resque']['database']);
-        
-        $container->register('packages.helper.plugin', 'Terramar\Packages\Helper\PluginHelper');
+
+        $container->register('packages.controller_manager', 'Terramar\Packages\Plugin\ControllerManager');
+
+        $container->register('packages.fragment_handler.inline_renderer', 'Symfony\Component\HttpKernel\Fragment\InlineFragmentRenderer')
+            ->addArgument(new Reference('http_kernel'))
+            ->addArgument(new Reference('event_dispatcher'));
+        $container->register('packages.fragment_handler', 'Symfony\Component\HttpKernel\Fragment\FragmentHandler')
+            ->addArgument(array(
+                    new Reference('packages.fragment_handler.inline_renderer')
+                ))
+            ->addArgument(false)
+            ->addMethodCall('setRequest', array(new Reference(
+                    'request',
+                    ContainerInterface::NULL_ON_INVALID_REFERENCE,
+                    false
+                )));
+
+        $container->register('packages.twig_extension.plugin', 'Terramar\Packages\Twig\PluginControllerExtension')
+            ->addArgument(new Reference('packages.controller_manager'))
+            ->addArgument(new Reference('packages.fragment_handler'))
+            ->addMethodCall('setRequest', array(new Reference(
+                    'request',
+                    ContainerInterface::NULL_ON_INVALID_REFERENCE,
+                    false
+                )));
+
+        $container->register('packages.fragment_handler.uri_signer', 'Symfony\Component\HttpKernel\UriSigner')
+            ->addArgument('');
+
+        $container->register('packages.fragment_handler.listener', 'Symfony\Component\HttpKernel\EventListener\FragmentListener')
+            ->addArgument(new Reference('packages.fragment_handler.uri_signer'))
+            ->addTag('kernel.event_subscriber');
+
+        $container->register('packages.helper.plugin', 'Terramar\Packages\Helper\PluginHelper')
+            ->addArgument(new Reference('packages.controller_manager'))
+            ->addArgument(new Reference('packages.fragment_handler'))
+            ->addMethodCall('setRequest', array(new Reference(
+                    'request',
+                    ContainerInterface::NULL_ON_INVALID_REFERENCE,
+                    false
+                )));
 
         $plugins = array();
         foreach ($this->plugins as $plugin) {
             $plugin->configure($container);
             $plugins[] = $plugin->getName();
         }
-        
+
         $container->setParameter('packages.registered_plugins', $plugins);
     }
 }
