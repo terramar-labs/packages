8c4297cd1187532bde27d773e0e2172aa60d0c17
Timothy Michael Bradley <fennasnogothrim@gmail.com>
2015-11-01 01:54:59 -0700

    Added more unit tests.

---
+++ tests/Event/PackageUpdateEventTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Terramar\Packages\Unit\Event;
+
+use Terramar\Packages\Entity\Remote;
+use Terramar\Packages\Event\RemoteEvent;
+
+class PackageUpdateEventTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var Remote */
+	private $remote;
+	
+	/** @var RemoteEvent */
+	private $sut;
+
+	public function setUp()
+	{
+		$this->remote = new Remote();
+		$this->sut = new RemoteEvent($this->remote);
+	}
+	
+	public function testGetRemote()
+	{
+		$this->assertSame($this->remote, $this->sut->getRemote());
+	}
+}
