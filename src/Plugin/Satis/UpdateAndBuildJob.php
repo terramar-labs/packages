d4b413404a7f39030291f613fc7bb4a39e0ae7ba
Bart Riepe <bart.riepe@nature.com>
2017-10-05 13:21:11 +0900

    Update single repository on push

--- src/Plugin/Satis/UpdateAndBuildJob.php
+++ src/Plugin/Satis/UpdateAndBuildJob.php
@@ -9,19 +9,40 @@
 
 namespace Terramar\Packages\Plugin\Satis;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\ProcessBuilder;
+use Terramar\Packages\Entity\Package;
 use Terramar\Packages\Job\ContainerAwareJob;
 
 class UpdateAndBuildJob extends ContainerAwareJob
 {
+    /**
+     * @return EntityManager
+     */
+    private function getEntityManager()
+    {
+        return $this->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
     public function run($args)
     {
+        /** @var Package $package */
+        $package = $this->getEntityManager()->getRepository(Package::class)->find($args['package_id']);
+
         $finder = new PhpExecutableFinder();
-        $builder = new ProcessBuilder(array('bin/console', 'satis:update', '--build', '--skip-errors'));
+        $builder = new ProcessBuilder([
+            'bin/console',
+            'satis:build',
+            '--repository-url',
+            $package->getSshUrl(),
+        ]);
+
         $builder->setEnv('HOME', $this->getContainer()->getParameter('app.root_dir'));
         $builder->setPrefix($finder->find());
         $builder->setTimeout(null);
+
+        echo $builder->getProcess()->getCommandLine() . "\n";
 
         $process = $builder->getProcess();
         $process->run(function ($type, $message) {
